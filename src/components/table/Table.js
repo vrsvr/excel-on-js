817778a1b99a5e8d39e5cebfee4e4de9e971ef24
vrsvr <rifatvarisov@gmail.com>
2021-01-06 18:29:07 +0500

    Ресайз строк и оптимизация

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,6 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import {$} from '@core/dom'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
@@ -16,7 +17,29 @@ export class Table extends ExcelComponent {
 
   onMousedown(event) {
     if (event.target.dataset.resize) {
-      console.log('Start resizing', event.target.dataset.resize)
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+      const type = $resizer.data.resize
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
+      document.onmousemove = e => {
+        if (type === 'col') {
+          const delta = e.pageX - coords.right
+          const value = coords.width + delta
+          $parent.$el.style.width = value + 'px'
+          cells.forEach(el => el.style.width = value + 'px')
+        } else {
+          const delta = e.pageY - coords.bottom
+          const value = coords.height + delta
+          $parent.$el.style.height = value + 'px'
+        }
+      }
+
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
     }
   }
 }
