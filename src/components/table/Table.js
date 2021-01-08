233efc962f576b0f2c077a588d3f9790bbf97797
vrsvr <rifatvarisov@gmail.com>
2021-01-08 10:42:44 +0500

    Кнопки (навигация)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,8 @@ import {ExcelComponent} from '@core/ExcelComponent'
 import {$} from '@core/dom'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from '@/components/table/table.resize'
-import {isCell, shouldResize, matrix} from '@/components/table/table.functions'
+// eslint-disable-next-line max-len
+import {isCell, shouldResize, matrix, nextSelector} from '@/components/table/table.functions'
 import {TableSelection} from '@/components/table/TableSelection'
 
 export class Table extends ExcelComponent {
@@ -10,7 +11,7 @@ export class Table extends ExcelComponent {
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown']
+      listeners: ['mousedown', 'keydown']
     })
   }
 
@@ -43,5 +44,26 @@ export class Table extends ExcelComponent {
       }
     }
   }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp'
+    ]
+
+    const {key} = event
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault()
+      const id = this.selection.current.id(true)
+      const $next = this.$root.find(nextSelector(key, id))
+      this.selection.select($next)
+    }
+  }
 }
+
 
--- src/core/dom.js
+++ src/core/dom.js
@@ -76,6 +76,11 @@ class Dom {
     return this.data.id
   }
 
+  focus() {
+    this.$el.focus()
+    return this
+  }
+
   addClass(className) {
     this.$el.classList.add(className)
   }
